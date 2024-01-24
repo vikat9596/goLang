37c691ca675859c27e17064096b5a2292423b21e
Vikat Rane <vikat9596@gmail.com>
2024-01-24 12:50:08 +0530

    Update controller.go

--- 25mongoapi/controller/controller.go
+++ 25mongoapi/controller/controller.go
@@ -15,7 +15,7 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-const connectionString = "mongodb+srv://vikat9596:hoqgYg-9nycwy-picgij@cluster0.mvrzkne.mongodb.net/?retryWrites=true&w=majority"
+const connectionString = "mongodb+srv://<username>:<password>@cluster0.mvrzkne.mongodb.net/?retryWrites=true&w=majority"
 const dbName = "netflix"
 const colName = "watchlist"
 
